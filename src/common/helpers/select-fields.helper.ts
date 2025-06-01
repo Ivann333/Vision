e8c9cf9be4723a90d8a8aae3ee41d5decd153487
Ivann333 <foxlissov@gmail.com>
2025-06-01 20:31:18 +0300

    fix: rename misleading error message and DTO parameter

--- src/common/helpers/select-fields.helper.ts
+++ src/common/helpers/select-fields.helper.ts
@@ -4,17 +4,17 @@ import { SelectFieldsQueryDto } from '../dto/select-fields-query.dto';
 
 export function applySelectFields<T>(
   query: Query<T[], T>,
-  sortQueryDto: SelectFieldsQueryDto,
+  selectQueryDto: SelectFieldsQueryDto,
   allowedFields: string[],
 ) {
-  if (!sortQueryDto.fields) return query;
-  const selectFields = sortQueryDto.fields.split(',');
+  if (!selectQueryDto.fields) return query;
+  const selectFields = selectQueryDto.fields.split(',');
 
   //validation
   for (const field of selectFields) {
     const cleanField = field.startsWith('-') ? field.slice(1) : field;
     if (!allowedFields.includes(cleanField)) {
-      throw new BadRequestException(`Invalid sort field: ${field}`);
+      throw new BadRequestException(`Invalid selected field: ${field}`);
     }
   }
 
