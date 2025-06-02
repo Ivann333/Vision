fcb3eebff523d36e87325ca701c1e8dff9d4c78a
Ivann333 <foxlissov@gmail.com>
2025-06-02 18:48:51 +0300

    feat: enhance app config with qs query parser and stricter validation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as qs from 'qs';
+import { AppModule } from './app.module';
 import { MongoExceptionFilter } from './global-filters/mongo-exception.filter';
 import { HttpExceptionFilter } from './global-filters/http-exception.filter';
 import { AllExceptionsFilter } from './global-filters/all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.set('query parser', qs.parse);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   app.useGlobalFilters(
     new AllExceptionsFilter(),
     new HttpExceptionFilter(),
