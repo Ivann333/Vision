9c5d4320898100fb8e32b3e8c2da32e7e578bfa0
Ivann333 <foxlissov@gmail.com>
2025-06-10 15:00:07 +0300

    feat: add global rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { TaskModule } from './task/task.module';
@@ -10,10 +12,25 @@ import { TimeEntryModule } from './time-entry/time-entry.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot({
+      errorMessage: 'Too many requests, please try again later.',
+      throttlers: [
+        {
+          ttl: 1000,
+          limit: 3,
+        },
+      ],
+    }),
     UserModule,
     DatabaseModule,
     TaskModule,
     TimeEntryModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
